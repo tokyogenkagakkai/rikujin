8a14c4d9bb835c10927b5502f327a8ca7a9301c3
KENZ <KENZ.gelsoft@gmail.com>
2018-09-09 21:15:25 +0900

    空亡の計算

--- rikujin.js
+++ rikujin.js
@@ -39,6 +39,12 @@ window.addEventListener('load', function () {
         var selected = e.target.value;
         var yorimiya = document.getElementById('yorimiya');
         yorimiya.value = calcYorimiya(selected);
+
+        updateKuubou();
+    }, false);
+    document.querySelector('#niti .si').addEventListener('change',
+    function (e) {
+        updateKuubou();
     }, false);
 
     document.querySelector('#getu .si').addEventListener('change',
@@ -89,4 +95,30 @@ function makeKansi(aId) {
     var si = div.querySelector('.si');
     setOptions(kan, jikkan);
     setOptions(si, junisi);
+}
+
+function updateKuubou() {
+    var nikkan = document.querySelector('#niti .kan').value;
+    var nissi = document.querySelector('#niti .si').value;
+    var kuubou = document.getElementById('kuubou');
+    kuubou.value = calcKuubou(nikkan, nissi).split('').join('・');
+}
+
+function calcKuubou(aNikkan, aNissi) {
+    var kuubous = ['子丑', '寅卯', '辰巳', '午未', '申酉', '戌亥'];
+    var syuntyus = [
+        ['甲寅', '乙卯', '丙辰', '丁巳', '戊午', '己未', '庚申', '辛酉', '壬戌', '癸亥'],
+        ['甲辰', '乙巳', '丙午', '丁未', '戊申', '己酉', '庚戌', '辛亥', '壬子', '癸丑'],
+        ['甲午', '乙未', '丙申', '丁酉', '戊戌', '己亥', '庚子', '辛丑', '壬寅', '癸卯'],
+        ['甲申', '乙酉', '丙戌', '丁亥', '戊子', '己丑', '庚寅', '辛卯', '壬辰', '癸巳'],
+        ['甲戌', '乙亥', '丙子', '丁丑', '戊寅', '己卯', '庚辰', '辛巳', '壬午', '癸未'],
+        ['甲子', '乙丑', '丙寅', '丁卯', '戊辰', '己巳', '庚午', '辛未', '壬申', '癸酉']
+    ];
+    for (var i = 0; i < syuntyus.length; ++i) {
+        var found = syuntyus[i].indexOf(aNikkan + aNissi);
+        if (found >= 0) {
+            return kuubous[i];
+        }
+    }
+    return null;
 }
